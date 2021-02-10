5bab614bd862a78bdba8b2d64fa6a8b283d333d0
satyadvaya <satyadvaya@gmail.com>
2021-02-10 15:06:10 -0800

    ready to submit

---
+++ app.js
@@ -0,0 +1,12 @@
+const nameInput = document.getElementById('name-input');
+const changeButton = document.getElementById('name-change-button');
+const nameDisplay = document.getElementById('name-display');
+
+changeButton.addEventListener('click', () => {
+    nameDisplay.textContent = nameInput.value;
+    nameInput.value = '';
+});
+
+// nameInput.addEventListener('mouseover', () => {
+//     console.log('moused over');
+// });
